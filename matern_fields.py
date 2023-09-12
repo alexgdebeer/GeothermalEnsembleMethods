2f921009a57c99c8374ce8c86f328fe045e01c05
Alex de Beer <adeb970@aucklanduni.ac.nz>
2023-09-12 20:15:59 +1200

    Clean up Matern field code

--- matern_fields.py
+++ matern_fields.py
@@ -2,6 +2,8 @@ import numpy as np
 from scipy import sparse
 from scipy.sparse import linalg
 from scipy.special import gamma
+import pyvista as pv
+import utils
 
 # Gradients of basis functions
 GRAD_2D = np.array([[-1.0, 1.0, 0.0], 
@@ -11,56 +13,55 @@ GRAD_3D = np.array([[-1.0, 1.0, 0.0, 0.0],
                     [-1.0, 0.0, 1.0, 0.0],
                     [-1.0, 0.0, 0.0, 1.0]])
 
-
 class MaternField2D():
 
-    def __init__(self):
+    def __init__(self, geo, mesh):
+
         self.dim = 2
         self.nu = 2 - self.dim / 2
 
-    def build_fem_matrices(self, points, elements, boundary_facets):
-        """
-        Builds the FEM matrices required for generating Matern fields in two
-        dimensions.
+        self.geo = geo
+        self.mesh = mesh
 
-        Parameters:
-        ----------------
-        points: np.ndarray 
-            Array of point locations. The ith row contains the coordinates of 
-            point i.
-        elements: np.ndarray 
-            Array of indices of points in each element. The ith row contains 
-            the indices of the points of element i.
-        boundary_facets: np.ndarray
-            Array of indices of points on each boundary facet. The ith row 
-            contains the indices of the points of facet i.
+        self.get_mesh_data()
+        self.build_fem_matrices()
 
-        Returns:
-        ----------------
-        M: scipy.sparse.lil_matrix
-            Mass matrix.
-        Kx: scipy.sparse.lil_matrix
-            Contributions to stiffness matrix in the x direction.
-        Ky: scipy.sparse.lil_matrix
-            Contributions to stiffness matrix in the y direction.
-        N: scipy.sparse.lil_matrix
-            Inner products of each pair of basis functions over the mesh 
-            boundary (used when implementing Robin boundary conditions).
-        """
-        
-        n_points = len(points)
+    def get_mesh_data(self):
+        """Extracts information on the points, elements and boundary facets 
+        of the mesh."""
 
-        M = sparse.lil_matrix((n_points, n_points))
-        Kx = sparse.lil_matrix((n_points, n_points))
-        Ky = sparse.lil_matrix((n_points, n_points))
-        N = sparse.lil_matrix((n_points, n_points))
+        self.mesh["point_inds"] = np.arange(self.mesh.n_points, dtype=np.int64)
 
-        for e in elements:        
+        self.n_points = self.mesh.n_points
+        self.points = self.mesh.points[:, :2]
+        self.elements = self.mesh.regular_faces
+
+        boundary = self.mesh.extract_feature_edges(boundary_edges=True, 
+                                                   feature_edges=False, 
+                                                   non_manifold_edges=False, 
+                                                   manifold_edges=False)
+
+        boundary_points = boundary.cast_to_pointset()["point_inds"]
+        boundary_facets = boundary.lines.reshape(-1, 3)[:, 1:]
+        self.boundary_facets = [boundary_points[f] for f in boundary_facets]
+
+    def build_fem_matrices(self):
+        """Builds the FEM matrices required for generating Matern fields in two
+        dimensions."""
+
+        utils.info("Constructing FEM matrices...")
+
+        M = sparse.lil_matrix((self.n_points, self.n_points))
+        Kx = sparse.lil_matrix((self.n_points, self.n_points))
+        Ky = sparse.lil_matrix((self.n_points, self.n_points))
+        N = sparse.lil_matrix((self.n_points, self.n_points))
+
+        for e in self.elements:
             
             for i in range(3):
                 
-                T = np.array([points[e[(i+1)%3]] - points[e[i]],
-                              points[e[(i+2)%3]] - points[e[i]]]).T
+                T = np.array([self.points[e[(i+1)%3]] - self.points[e[i]],
+                              self.points[e[(i+2)%3]] - self.points[e[i]]]).T
                 detT = np.abs(np.linalg.det(T))
                 invT = np.linalg.inv(T)
 
@@ -77,18 +78,20 @@ class MaternField2D():
                     Kx[e[i], e[j]] += kl.flat[0] * kr.flat[0]
                     Ky[e[i], e[j]] += kl.flat[1] * kr.flat[1]
 
-        for (fi, fj) in boundary_facets:
-            n = np.linalg.norm(points[fi] - points[fj])
+        for (fi, fj) in self.boundary_facets:
+            n = np.linalg.norm(self.points[fi] - self.points[fj])
             N[fi, fi] += n * 1/3
             N[fj, fj] += n * 1/3
             N[fi, fj] += n * 1/6
             N[fj, fi] += n * 1/6
 
         self.M = M
-        self.L = np.linalg.cholesky(M.toarray())
+        self.L = np.linalg.cholesky(M.toarray()) # TODO: sparse cholesky?
         self.Kx = Kx 
         self.Ky = Ky
         self.N = N
+
+        utils.info("FEM matrices constructed.")
 
     def generate_field(self, W, sigma, lx, ly, bcs="robin", lam=None):
         """Given a set of white noise and a set of hyperparameters,
@@ -115,33 +118,67 @@ class MaternField2D():
 
         X = linalg.spsolve(self.H, np.sqrt(alpha * lx * ly) * self.L.T @ W)
         return X
+    
+    def plot(self, **kwargs):
+        """Generates a 3D visualisation of the mesh using PyVista."""
+        p = pv.Plotter()
+        p.add_mesh(self.mesh, **kwargs)
+        p.show()
+    
+    def layer_plot(self, values, **kwargs):
+        """Generates a visualisation of the mesh using Layermesh."""
+        col_values = [values[c.column.index] for c in self.geo.cell]
+        self.geo.layer_plot(value=col_values, **kwargs)
 
 
 class MaternField3D():
 
-    def __init__(self):
+    def __init__(self, geo, mesh):
+
         self.dim = 3
         self.nu = 2 - self.dim / 2
+
+        self.geo = geo
+        self.mesh = mesh 
+
+        self.get_mesh_data()
+        self.build_fem_matrices()
+        self.build_geo_to_mesh_mapping()
         
-    def build_fem_matrices(self, points, elements, boundary_facets):
+    def get_mesh_data(self):
+        """Extracts information on the points, elements and facets of the 
+        mesh."""
+
+        self.mesh["point_inds"] = np.arange(self.mesh.n_points, dtype=np.int64)
+
+        self.n_points = self.mesh.n_points 
+        self.points = self.mesh.points 
+        self.elements = self.mesh.cells_dict[10]
+
+        boundary = self.mesh.extract_geometry()
+        boundary_points = boundary.cast_to_pointset()["point_inds"]
+        boundary_facets = boundary.faces.reshape(-1, 4)[:, 1:]
+        self.boundary_facets = [boundary_points[f] for f in boundary_facets]
+
+    def build_fem_matrices(self):
         """Builds the FEM matrices required to generate Matern fields in three 
         dimensions."""
 
-        n_points = len(points)
+        utils.info("Constructing FEM matrices...")
 
-        M = sparse.lil_matrix((n_points, n_points))
-        Kx = sparse.lil_matrix((n_points, n_points))
-        Ky = sparse.lil_matrix((n_points, n_points))
-        Kz = sparse.lil_matrix((n_points, n_points))
-        N = sparse.lil_matrix((n_points, n_points))
+        M = sparse.lil_matrix((self.n_points, self.n_points))
+        Kx = sparse.lil_matrix((self.n_points, self.n_points))
+        Ky = sparse.lil_matrix((self.n_points, self.n_points))
+        Kz = sparse.lil_matrix((self.n_points, self.n_points))
+        N = sparse.lil_matrix((self.n_points, self.n_points))
 
-        for e in elements:
+        for e in self.elements:
 
             for i in range(4):
 
-                T = np.array([points[e[(i+1)%4]] - points[e[i]],
-                              points[e[(i+2)%4]] - points[e[i]],
-                              points[e[(i+3)%4]] - points[e[i]]]).T
+                T = np.array([self.points[e[(i+1)%4]] - self.points[e[i]],
+                              self.points[e[(i+2)%4]] - self.points[e[i]],
+                              self.points[e[(i+3)%4]] - self.points[e[i]]]).T
 
                 detT = np.abs(np.linalg.det(T))
                 invT = np.linalg.inv(T)
@@ -160,15 +197,12 @@ class MaternField3D():
                     Ky[e[i], e[j]] += kl.flat[1] * kr.flat[1]
                     Kz[e[i], e[j]] += kl.flat[2] * kr.flat[2]
         
-        for f in boundary_facets:
+        for f in self.boundary_facets:
 
             for i in range(3):
                 
-                # Calculate the determinant of the transformation from the 
-                # reference facet to the new facet (no division by 1/2
-                # because the reference facet has an area of 1/2)
-                detTb = np.linalg.norm(np.cross(points[f[(i+1)%3]] - points[f[i]], 
-                                                points[f[(i+2)%3]] - points[f[i]]))
+                detTb = np.linalg.norm(np.cross(self.points[f[(i+1)%3]] - self.points[f[i]], 
+                                                self.points[f[(i+2)%3]] - self.points[f[i]]))
 
                 for j in range(3):
                     if i == j:
@@ -182,6 +216,36 @@ class MaternField3D():
         self.Ky = Ky 
         self.Kz = Kz 
         self.N = N
+
+        utils.info("FEM matrices constructed.")
+
+    def build_geo_to_mesh_mapping(self):
+        """Generates an operator that maps the result from the FEM mesh back to 
+        the cells in the model geometry."""
+
+        utils.info("Constructing geo to mesh mapping...")
+
+        B = sparse.lil_matrix((self.geo.num_cells, self.mesh.n_points))
+        elements = self.mesh.find_containing_cell([c.centre for c in self.geo.cell])
+
+        for c, e in zip(self.geo.cell, elements):
+            
+            ps = self.elements[e]
+
+            # Map cell centre back to the reference triangle
+            T = np.array([self.points[ps[1]] - self.points[ps[0]],
+                          self.points[ps[2]] - self.points[ps[0]],
+                          self.points[ps[3]] - self.points[ps[0]]]).T
+            
+            x, y, z = np.linalg.inv(T) @ (c.centre - self.points[ps[0]])
+            B[c.index, ps[0]] = 1 - x - y - z
+            B[c.index, ps[1]] = x
+            B[c.index, ps[2]] = y
+            B[c.index, ps[3]] = z
+
+        self.B = B
+
+        utils.info("geo to mesh mapping constructed.")
 
     def generate_field(self, W, sigma, lx, ly, lz, bcs="robin", lam=None):
         """Generates a Matern field."""
@@ -205,9 +269,17 @@ class MaternField3D():
         # cov = alpha * lx * ly * lz * inv_H @ self.M.toarray() @ inv_H.T
         # return np.sqrt(np.diag(cov))
 
-        X = linalg.spsolve(self.H, np.sqrt(alpha * lx * ly * lz) * self.L.T @ W)
+        b = np.sqrt(alpha * lx * ly * lz) * self.L.T @ W
+        X = linalg.spsolve(self.H, b)
         return X
     
+    def plot(self, **kwargs):
+        p = pv.Plotter()
+        p.add_mesh(self.mesh, **kwargs)
+        p.show()
+
+    def slice_plot(self, values, **kwargs):
+        self.geo.slice_plot(value=self.B@values, **kwargs)
 
 def generate_fem_matrices_2D(points, elements, boundary_facets):
     """
@@ -317,10 +389,7 @@ def generate_fem_matrices_3D(points, elements, boundary_facets):
     for f in boundary_facets:
 
         for i in range(3):
-            
-            # Calculate the determinant of the transformation from the 
-            # reference facet to the new facet (no division by 1/2
-            # because the reference facet has an area of 1/2)
+
             detTb = np.linalg.norm(np.cross(points[f[(i+1)%3]] - points[f[i]], 
                                             points[f[(i+2)%3]] - points[f[i]]))
 
@@ -331,29 +400,3 @@ def generate_fem_matrices_3D(points, elements, boundary_facets):
                     N[f[i], f[j]] += detTb * 1/24
 
     return M, Kx, Ky, Kz, N
-
-
-def generate_mesh_mapping_3D(mesh, geo):
-    """Generates an operator that maps the result from the FEM mesh back to 
-    the cells in the model geometry."""
-
-    H = sparse.lil_matrix((geo.num_cells, mesh.n_points))
-    elements = mesh.find_containing_cell([c.centre for c in geo.cell])
-
-    for c, e in zip(geo.cell, elements):
-        
-        ps = mesh.cells_dict[10][e]
-
-        # Map cell centre back to the reference triangle
-        T = np.array([mesh.points[ps[1]] - mesh.points[ps[0]],
-                      mesh.points[ps[2]] - mesh.points[ps[0]],
-                      mesh.points[ps[3]] - mesh.points[ps[0]]]).T
-        
-        x, y, z = np.linalg.inv(T) @ (c.centre - mesh.points[ps[0]])
-
-        H[c.index, ps[0]] = 1 - x - y - z
-        H[c.index, ps[1]] = x
-        H[c.index, ps[2]] = y
-        H[c.index, ps[3]] = z
-
-    return H
--- test_pyvista_2D.py
+++ test_pyvista_2D.py
@@ -8,42 +8,19 @@ MESH_PATH = "models/channel/gCH"
 geo = lm.mesh(f"{MESH_PATH}.h5")
 mesh = pv.PolyData([[*col.centre, 0.0] for col in geo.column]).delaunay_2d()
 # mesh = pv.UnstructuredGrid("test/mesh_tri.msh").delaunay_2d().scale(10)
-# mesh.plot(show_edges=True)
 
-mesh["point_indices"] = np.arange(mesh.n_points, dtype=np.int32)
-
-points = mesh.points[:, :2]
-elements = mesh.regular_faces
-
-boundary = mesh.extract_feature_edges(boundary_edges=True, 
-                                      feature_edges=False, 
-                                      non_manifold_edges=False, 
-                                      manifold_edges=False)
-
-boundary_points = boundary.cast_to_pointset()["point_indices"]
-boundary_facets = boundary.lines.reshape(-1, 3)[:, 1:]
-boundary_facets = np.array([boundary_points[f] for f in boundary_facets])
-
-# p = pv.Plotter()
-# p.add_mesh(mesh, show_edges=True)
-# for line in boundary_facets:
-#     p.add_points(mesh.points[line])
-# p.show()
-
-matern_field = MaternField2D()
-matern_field.build_fem_matrices(points, elements, boundary_facets)
+matern_field = MaternField2D(geo, mesh)
 
 sigma = 1.0
 lx = 150
 ly = 150
 lam = 1.42 * np.sqrt(lx * ly) # TODO: tune Robin parameter
 
-W = np.random.normal(loc=0.0, scale=1.0, size=mesh.n_points)
+W = np.random.normal(loc=0.0, scale=1.0, size=matern_field.n_points)
 X = matern_field.generate_field(W, sigma, lx, ly, bcs="robin")
 
-p = pv.Plotter()
-p.add_mesh(mesh, show_edges=True, scalars=X, cmap="turbo")
-p.show()
+matern_field.plot(show_edges=True, scalars=X, cmap="turbo")
+matern_field.layer_plot(values=X, colourmap="turbo")
 
 # XS = np.zeros((mesh.n_points, 10_000))
 
@@ -56,14 +33,3 @@ p.show()
 #         print(i)
 
 # stds = np.std(XS, axis=1)
-
-# p = pv.Plotter()
-# p.add_mesh(mesh, show_edges=True, scalars=stds, cmap="turbo")
-# p.show()
-
-def plot_col_values(geo, values, **kwargs):
-    """Plots a set of values that correspond to the columns of the mesh."""
-    col_values = np.array([values[cell.column.index] for cell in geo.cell])
-    geo.layer_plot(value=col_values, **kwargs)
-
-plot_col_values(geo, X, colourmap="turbo")
