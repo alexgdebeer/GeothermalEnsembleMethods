7ef3a73982e19a0271a4a39c889465fa8d16ae86
alexgdebeer <adeb970@aucklanduni.ac.nz>
2023-09-19 15:54:33 +1200

    Add a clay cap class

--- clay_cap.py
+++ clay_cap.py
@@ -2,41 +2,58 @@ import itertools as it
 from layermesh import mesh as lm
 import numpy as np
 import pyvista as pv
-import GeothermalEnsembleMethods as gm
+from GeothermalEnsembleMethods import utils
 
 MESH_NAME = "models/channel/gCH"
 
-# Fourier stuff
-N_TERMS = 5
-COEF_SDS = 5
+class ClayCap():
 
-@gm.timer
-def get_cap_radii(cell_centres, cap_centre, width_h, width_v, dip, cs):
-    """Returns the cells that are part of the clay cap."""
+    def __init__(self, n_terms, coef_sds):
+        self.n_terms = n_terms 
+        self.coef_sds = coef_sds
 
-    ds = cell_centres - cap_centre
+    def cartesian_to_spherical(self, ds):
+        rs = np.linalg.norm(ds, axis=1)
+        phis = np.arccos(ds[:, 2] / rs)
+        thetas = np.arctan2(ds[:, 1], ds[:, 0])
+        return rs, phis, thetas
+    
+    def compute_cap_radii(self, phis, thetas, width_h, width_v, coefs):
+        """Computes the radius of the clay cap in the direction of each cell, 
+        by taking the radius of the (deformed) ellipse that forms the base
+        of the cap, then adding the randomised Fourier series to it."""
 
-    # Give cap a curved appearance
-    # TODO: should this be width_h, or mean(width_h)?
-    ds[:, 2] += (dip / width_h**2) * (ds[:, 0]**2 + ds[:, 1]**2) 
-
-    rs = np.linalg.norm(ds, axis=1)
-    phis = np.arccos(ds[:, 2] / rs)
-    thetas = np.arctan2(ds[:, 1], ds[:, 0])
-
-    rs_cap = np.sqrt(((np.sin(phis) * np.cos(thetas) / width_h)**2 + \
+        rs = np.sqrt(((np.sin(phis) * np.cos(thetas) / width_h)**2 + \
                       (np.sin(phis) * np.sin(thetas) / width_h)**2 + \
                       (np.cos(phis) / width_v)**2) ** -1)
-    
-    for n, m in it.product(range(N_TERMS), range(N_TERMS)):
         
-        rs_cap += cs[n, m, 0] * np.cos(n * thetas) * np.cos(m * phis) + \
-                  cs[n, m, 1] * np.cos(n * thetas) * np.sin(m * phis) + \
-                  cs[n, m, 2] * np.sin(n * thetas) * np.cos(m * phis) + \
-                  cs[n, m, 3] * np.sin(n * thetas) * np.sin(m * phis)
+        for n, m in it.product(range(self.n_terms), range(self.n_terms)):
         
-    cap = rs < rs_cap
-    return cap
+            rs += coefs[n, m, 0] * np.cos(n * thetas) * np.cos(m * phis) + \
+                  coefs[n, m, 1] * np.cos(n * thetas) * np.sin(m * phis) + \
+                  coefs[n, m, 2] * np.sin(n * thetas) * np.cos(m * phis) + \
+                  coefs[n, m, 3] * np.sin(n * thetas) * np.sin(m * phis)
+        
+        return rs
+
+    @utils.timer
+    def get_containing_cells(self, cell_centres, cap_centre, 
+                             width_h, width_v, dip, coefs):
+        """Returns an array of booleans that indicate whether each cell is 
+        contained within the clay cap."""
+
+        ds = cell_centres - cap_centre
+
+        # Give cap a curved appearance
+        # TODO: should this be width_h, or mean(width_h)?
+        ds[:, -1] += (dip / width_h**2) * (ds[:, 0]**2 + ds[:, 1]**2) 
+
+        cell_radii, cell_phis, cell_thetas = self.cartesian_to_spherical(ds)
+
+        cap_radii = self.compute_cap_radii(cell_phis, cell_thetas,
+                                           width_h, width_v, coefs)
+        
+        return cell_radii < cap_radii
 
 def plot(mesh, geo, cap):
     """Generates a 3D plot of the cells that make up the clay cap."""
@@ -52,6 +69,8 @@ def plot(mesh, geo, cap):
     p.add_mesh(mesh.threshold([-0.5, 0.5]),  opacity=0.5, cmap="coolwarm")
     p.show()
 
+clay_cap = ClayCap()
+
 geo = lm.mesh(f"{MESH_NAME}.h5")
 mesh = pv.UnstructuredGrid(f"{MESH_NAME}.vtu")
 
@@ -64,9 +83,11 @@ width_h, width_v = 475, 50
 dip = np.random.uniform(50, 200) # Average difference in height between the centre of the cap and the edges 
 
 # Fourier coefficients
-cs = np.random.normal(loc=0.0, scale=COEF_SDS, size=(N_TERMS, N_TERMS, 4))
+coefs = np.random.normal(loc=0.0, scale=clay_cap.coef_sds,
+                         size=(clay_cap.n_terms, clay_cap.n_terms, 4))
 
-cap = get_cap_radii(cell_centres, cap_centre, width_h, width_v, dip, cs)
+cap = clay_cap.get_containing_cells(cell_centres, cap_centre, 
+                                    width_h, width_v, dip, coefs)
 
 plot(mesh, geo, cap)
 # geo.slice_plot("y", value=cap, colourmap="coolwarm")
