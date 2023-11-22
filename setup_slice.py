f9415bc3f51beab1f60f529abf4c1d246706eafd
Alex de Beer <adeb970@aucklanduni.ac.nz>
2023-11-22 14:20:06 +1300

    Add DataHandler object

--- setup_slice.py
+++ setup_slice.py
@@ -1,26 +1,26 @@
 """Setup script for 2D vertical slice model."""
 
 import numpy as np
-from scipy import stats
+from scipy import sparse, stats
 
 from src.consts import SECS_PER_WEEK
 from src.grfs import *
 from src.models import *
 
-np.random.seed(11) # 16, 38 not bad
+np.random.seed(1) # 11 not bad
 
 DATA_FOLDER = "data/slice"
 MODEL_FOLDER = "models/slice"
 
-WS_TRUE_PATH = f"{DATA_FOLDER}/ws_true.txt"
-PS_TRUE_PATH = f"{DATA_FOLDER}/ps_true.txt"
-FS_TRUE_PATH = f"{DATA_FOLDER}/Fs_true.txt"
-GS_TRUE_PATH = f"{DATA_FOLDER}/Gs_true.txt"
-OBS_PATH = f"{DATA_FOLDER}/obs.txt"
-COV_PATH = f"{DATA_FOLDER}/C_e.txt" # .npy?
+W_TRUE_PATH = f"{DATA_FOLDER}/w_true.npz"
+P_TRUE_PATH = f"{DATA_FOLDER}/p_true.npz"
+F_TRUE_PATH = f"{DATA_FOLDER}/F_true.npz"
+G_TRUE_PATH = f"{DATA_FOLDER}/G_true.npz"
+OBS_PATH = f"{DATA_FOLDER}/obs.npz"
+COV_PATH = f"{DATA_FOLDER}/C_e.npz" # .npy?
 
 """
-Prior
+Classes
 """
 
 class SlicePrior():
@@ -99,6 +99,139 @@ class SlicePrior():
     def sample(self, n=1):
         return np.random.normal(size=(self.n_params, n))
 
+class DataHandler():
+
+    def __init__(self, mesh: SliceMesh, temp_obs_xs, temp_obs_zs,
+                 prod_obs_ts, tmax, nt):
+
+        self.mesh = mesh
+
+        self.n_wells = len(well_xs)
+        self.tmax = tmax 
+        self.ts = np.linspace(0, tmax, nt+1)
+        self.nt = nt
+        
+        # Temperature observation coordinates
+        self.temp_obs_xs = temp_obs_xs
+        self.temp_obs_zs = temp_obs_zs
+        self.temp_obs_cs = np.array([[x, z] for x in temp_obs_xs 
+                                     for z in temp_obs_zs])
+        
+        # Production observation times
+        self.prod_obs_ts = prod_obs_ts
+        self.inds_prod_obs = np.searchsorted(self.ts, prod_obs_ts-EPS)
+
+        self.n_prod_obs_ts = len(self.prod_obs_ts)
+
+        self.n_temp_full = self.mesh.nx * self.mesh.nz 
+        self.n_pres_full = (self.nt+1) * self.n_wells
+        self.n_enth_full = (self.nt+1) * self.n_wells
+
+        self.n_temp_obs = len(self.temp_obs_cs)
+        self.n_pres_obs = self.n_prod_obs_ts * self.n_wells
+        self.n_enth_obs = self.n_prod_obs_ts * self.n_wells
+
+        self.generate_inds_full()
+        self.generate_inds_obs()
+
+    def generate_inds_full(self):
+        """Generates indices used to extract temperatures, pressures 
+        and enthalpies from a vector of complete data."""
+        self.inds_full_temp = np.arange(self.n_temp_full)
+        self.inds_full_pres = np.arange(self.n_pres_full) + 1 + self.inds_full_temp[-1]
+        self.inds_full_enth = np.arange(self.n_enth_full) + 1 + self.inds_full_pres[-1]
+
+    def generate_inds_obs(self):
+        """Generates indices used to extract temperatures, pressures 
+        and enthalpy observations from a vector of observations."""
+        self.inds_obs_temp = np.arange(self.n_temp_obs)
+        self.inds_obs_pres = np.arange(self.n_pres_obs) + 1 + self.inds_obs_temp[-1]
+        self.inds_obs_enth = np.arange(self.n_enth_obs) + 1 + self.inds_obs_pres[-1]
+
+    def get_full_temperatures(self, F_i):
+        """Extracts the temperatures from a set of data."""
+        temp = F_i[self.inds_full_temp]
+        temp = np.reshape(temp, (self.mesh.nz, self.mesh.nx)) # TODO: check
+        return temp
+    
+    def get_full_pressures(self, F_i):
+        """Extracts the pressures from a complete set of data."""
+        pres = F_i[self.inds_full_pres]
+        pres = np.reshape(pres, (self.n_wells, -1)) # TODO: check
+        return pres
+
+    def get_full_enthalpies(self, F_i):
+        """Extracts the enthalpies from a complete set of data."""
+        enth = F_i[self.inds_full_enth]
+        enth = np.reshape(enth, (self.n_wells, -1)) # TODO: check
+        return enth
+    
+    def get_full_states(self, F_i):
+        temp = self.get_full_temperatures(F_i)
+        pres = self.get_full_pressures(F_i)
+        enth = self.get_full_enthalpies(F_i)
+        return temp, pres, enth 
+    
+    def get_obs_temperatures(self, temp_full):
+        """Extracts the temperatures at each observation point from a 
+        full set of temperatures."""
+        mesh_coords = (self.mesh.xs, self.mesh.zs)
+        interpolator = RegularGridInterpolator(mesh_coords, temp_full.T)
+        temp_obs = interpolator(self.temp_obs_cs) # TODO: check
+        return self.reshape_obs(temp_obs)
+    
+    def get_obs_pressures(self, pres_full):
+        """Extracts the pressures at each observation location from a 
+        full set of pressures."""
+        pres_obs = pres_full[:, self.inds_prod_obs]
+        return self.reshape_obs(pres_obs)
+
+    def get_obs_enthalpies(self, enth_full):
+        """Extracts the enthalpies at each observation location from a
+        full set of enthalpies."""
+        enth_obs = enth_full[:, self.inds_prod_obs]
+        return self.reshape_obs(enth_obs)
+    
+    def get_obs_states(self, F_i):
+        """Extracts the observations from a complete vector of model 
+        output, and returns each type of observation individually."""
+        temp_full, pres_full, enth_full = self.get_full_states(F_i)
+        temp_obs = self.get_obs_temperatures(temp_full)
+        pres_obs = self.get_obs_pressures(pres_full)
+        enth_obs = self.get_obs_enthalpies(enth_full)
+        return temp_obs, pres_obs, enth_obs
+    
+    def get_obs(self, F_i):
+        """Extracts the observations from a complete vector of model
+        output, and returns them as a vector."""
+        temp_obs, pres_obs, enth_obs = self.get_obs_states(F_i)
+        obs = np.concatenate((temp_obs.flatten(), 
+                              pres_obs.flatten(), 
+                              enth_obs.flatten()))
+        return obs
+    
+    def reshape_obs(self, obs):
+        """Reshapes observations 2D arrays such that each row contains
+        the observations for a single well."""
+        return np.reshape(obs, (self.n_wells, -1))
+
+    def split_obs(self, G_i):
+        """Splits a set of observations into temperatures, pressures 
+        and enthalpies."""
+        temp_obs = self.reshape_obs(G_i[self.inds_obs_temp])
+        pres_obs = self.reshape_obs(G_i[self.inds_obs_pres])
+        enth_obs = self.reshape_obs(G_i[self.inds_obs_enth])
+        return temp_obs, pres_obs, enth_obs
+    
+    def downhole_temps(self, temps):
+        """Generates the downhole temperatures for a given well.
+        TODO: cut off at well depths?"""
+        mesh_coords = (self.mesh.xs, self.mesh.zs)
+        interpolator = RegularGridInterpolator(mesh_coords, temps.T)
+        well_coords = np.array([[x, z] for x in self.temp_obs_xs for z in self.mesh.zs])
+        temp_well = interpolator(well_coords)
+        return self.reshape_obs(temp_well)
+
 """
 Meshes
 """
@@ -109,27 +242,27 @@ zmax = 1500.0
 
 nx_fine, nz_fine = 35, 35
 nx_crse, nz_crse = 25, 25
-ny = 1 # can probably get rid of this
 
 tmax, nt = 104.0 * SECS_PER_WEEK, 24
 dt = tmax / nt
 
-n_blocks_fine = nx_fine * ny * nz_fine
-n_blocks_crse = nx_crse * ny * nz_crse
+n_blocks_fine = nx_fine * nz_fine
+n_blocks_crse = nx_crse * nz_crse
 
 mesh_name_fine = f"{MODEL_FOLDER}/gSL{n_blocks_fine}"
 mesh_name_crse = f"{MODEL_FOLDER}/gSL{n_blocks_crse}"
 model_name_fine = f"{MODEL_FOLDER}/SL{n_blocks_fine}"
 model_name_crse = f"{MODEL_FOLDER}/SL{n_blocks_crse}"
 
-mesh_fine = RegularMesh(mesh_name_fine, xmax, ymax, zmax, nx_fine, ny, nz_fine)
-mesh_crse = RegularMesh(mesh_name_crse, xmax, ymax, zmax, nx_crse, ny, nz_crse)
+mesh_fine = SliceMesh(mesh_name_fine, xmax, ymax, zmax, nx_fine, nz_fine)
+mesh_crse = SliceMesh(mesh_name_crse, xmax, ymax, zmax, nx_crse, nz_crse)
 
 """
 Wells and feedzones
 """
 
-# TODO: make simpler
+# TODO: this section can surely be made simpler
+
 well_xs = [200, 475, 750, 1025, 1300]
 n_wells = len(well_xs)
 well_depths = [-1300] * n_wells
@@ -151,68 +284,20 @@ upflow_cell_fine = mesh_fine.m.find(upflow_loc_fine)
 upflow_cell_crse = mesh_crse.m.find(upflow_loc_crse)
 
 """
-Observation times and locations
+Observations
 """
 
-# Natural state temperature observation locations
-well_temp_depths = [-300, -500, -700, -900, -1100, -1300]
-ns_temp_coords = np.array([[x, z] for x in well_xs for z in well_temp_depths])
+temp_obs_zs = np.array([-300, -500, -700, -900, -1100, -1300])
+prod_obs_ts = np.array([0, 13, 26, 39, 52]) * SECS_PER_WEEK
 
-# Pressure and enthalpy observation times
-pr_obs_times = np.array([0, 13, 26, 39, 52]) * SECS_PER_WEEK
-
-n_well_temp_depths = len(well_temp_depths)
-n_pr_obs_times = len(pr_obs_times)
-
-n_temp_obs = n_well_temp_depths * n_wells
-n_pres_obs = n_pr_obs_times * n_wells
-n_enth_obs = n_pr_obs_times * n_wells
-
-"""
-Constants and functions for extracting data
-"""
-
-# # Indices for extracting each state variable from the complete data
-# temp_raw_inds_fine = np.arange(n_blocks_fine)
-# pres_raw_inds_fine = np.arange(n_wells * (nt+1)) + temp_raw_inds_fine[-1] + 1
-# enth_raw_inds_fine = np.arange(n_wells * (nt+1)) + pres_raw_inds_fine[-1] + 1
-
-# temp_raw_inds_crse = np.arange(n_blocks_crse)
-# pres_raw_inds_crse = np.arange(n_wells * (nt+1)) + temp_raw_inds_crse[-1] + 1
-# enth_raw_inds_crse = np.arange(n_wells * (nt+1)) + pres_raw_inds_crse[-1] + 1
-
-# # Indices for extracting each observations of each variable from the data
-
-# def unpack_data_raw(Fs, mesh=mesh_crse, 
-#                     temp_inds=temp_raw_inds_crse,
-#                     pres_inds=pres_raw_inds_crse,
-#                     enth_inds=enth_raw_inds_crse):
-#     """Extracts the temperatures, pressures and enthalpies from a 
-#     model run."""
-
-#     temp = np.reshape(Fs[temp_inds], (mesh.nx, mesh.nz))
-#     pres = np.reshape(Fs[pres_inds], (nt+1, n_wells))
-#     enth = np.reshape(Fs[enth_inds], (nt+1, n_wells))
-#     return temp, pres, enth
-
-temp_obs_inds = np.arange(n_temp_obs)
-pres_obs_inds = np.arange(n_pres_obs) + temp_obs_inds[-1] + 1
-enth_obs_inds = np.arange(n_enth_obs) + pres_obs_inds[-1] + 1
-
-def unpack_data_obs(Gs):
-    """Extracts the temperatures, pressures and enthalpies from a 
-    set of predictions."""
-
-    temp = np.reshape(Gs[temp_obs_inds], (n_well_temp_depths, n_wells))
-    pres = np.reshape(Gs[pres_obs_inds], (n_pr_obs_times, n_wells))
-    enth = np.reshape(Gs[enth_obs_inds], (n_pr_obs_times, n_wells))
-    return temp, pres, enth
+data_handler_crse = DataHandler(mesh_crse, well_xs, temp_obs_zs, prod_obs_ts, tmax, nt)
+data_handler_fine = DataHandler(mesh_fine, well_xs, temp_obs_zs, prod_obs_ts, tmax, nt)
 
 """
 Model functions
 """
 
-def G(p_i, mesh=mesh_crse, model_name=model_name_crse, 
+def F(p_i, mesh=mesh_crse, model_name=model_name_crse, 
       wells=wells_crse, upflow_cell=upflow_cell_crse):
     """Given a set of transformed parameters, forms and runs the 
     corresponding model, then returns the full model output and model
@@ -221,14 +306,14 @@ def G(p_i, mesh=mesh_crse, model_name=model_name_crse,
     *logks, upflow_rate = p_i
     upflows = [MassUpflow(upflow_cell, upflow_rate)]
     
-    model = Model2D(model_name, mesh, logks, wells, upflows, 
-                    dt, tmax, ns_temp_coords, pr_obs_times)
+    model = Model2D(model_name, mesh, logks, wells, upflows, dt, tmax)
 
     if (flag := model.run()) == ExitFlag.FAILURE: 
         return flag
-    
-    F_i, G_i = model.get_pr_data()
-    return F_i, G_i
+    return model.get_pr_data()
+
+def G(F_i, data_handler=data_handler_crse):
+    return data_handler.get_obs(F_i)
 
 """
 Prior
@@ -281,57 +366,61 @@ Truth
 
 NOISE_LEVEL = 0.05 # Noise level as a percentage of the maximum of the raw data
 
-def generate_data(G_t):
-
-    temp_true, pres_true, enth_true = unpack_data_obs(G_t)
-
-    cov_temp = (NOISE_LEVEL * np.max(temp_true)) ** 2 * np.eye(temp_true.size)
-    cov_pres = (NOISE_LEVEL * np.max(pres_true)) ** 2 * np.eye(pres_true.size)
-    cov_enth = (NOISE_LEVEL * np.max(enth_true)) ** 2 * np.eye(enth_true.size)
-
-    C_e = sparse.block_diag((cov_temp, cov_pres, cov_enth)).toarray()
-    y = np.random.multivariate_normal(G_t, C_e)
-    return y, C_e
-
-def generate_truth(mesh, model_name, wells, upflow_cell):
+def generate_truth(mesh: SliceMesh, model_name, wells, upflow_cell):
     """Generates the truth and observations using the fine model."""
     
     truth_dist = generate_prior(mesh, upflow_cell)
 
-    ws_t = truth_dist.sample()
-    ps_t = truth_dist.transform(ws_t)
+    w_t = truth_dist.sample()
+    p_t = truth_dist.transform(w_t)
 
     # TEMP: sanity check
-    *ks_t, _ = ps_t
-    mesh.m.slice_plot(value=ks_t, colourmap="viridis")
+    mesh.m.slice_plot(value=p_t[:-1], colourmap="viridis")
 
-    Fs_t, Gs_t = G(ps_t, mesh, model_name, wells, upflow_cell)
+    F_t = F(p_t, mesh, model_name, wells, upflow_cell)
+    G_t = G(F_t, data_handler_fine)
 
-    temp_true = Fs_t[:(mesh.nx * mesh.nz)]
+    temp_true = F_t[:(mesh.nx * mesh.nz)]
     mesh.m.slice_plot(value=temp_true, colourmap="coolwarm")
 
-    y, C_e = generate_data(Gs_t)
+    np.save(W_TRUE_PATH, w_t)
+    np.save(P_TRUE_PATH, p_t)
+    np.save(F_TRUE_PATH, F_t)
+    np.save(G_TRUE_PATH, G_t)
 
-    np.savetxt(WS_TRUE_PATH, ws_t)
-    np.savetxt(PS_TRUE_PATH, ps_t)
-    np.savetxt(FS_TRUE_PATH, Fs_t)
-    np.savetxt(GS_TRUE_PATH, Gs_t)
-    np.savetxt(OBS_PATH, y)
-    np.savetxt(COV_PATH, C_e)
-
-    return ws_t, ps_t, Fs_t, Gs_t, y, C_e
+    return w_t, p_t, F_t, G_t
 
 def read_truth():
 
-    ws_t = np.genfromtxt(WS_TRUE_PATH)
-    ps_t = np.genfromtxt(PS_TRUE_PATH)
-    Fs_t = np.genfromtxt(FS_TRUE_PATH)
-    Gs_t = np.genfromtxt(GS_TRUE_PATH)
-    y = np.genfromtxt(OBS_PATH)
-    C_e = np.genfromtxt(COV_PATH)
+    w_t = np.load(W_TRUE_PATH)
+    p_t = np.load(P_TRUE_PATH)
+    F_t = np.load(F_TRUE_PATH)
+    G_t = np.load(G_TRUE_PATH)
     
-    return ws_t, ps_t, Fs_t, Gs_t, y, C_e
+    return w_t, p_t, F_t, G_t
 
-ws_t, ps_t, Fs_t, Gs_t, y, C_e = generate_truth(
-    mesh_fine, model_name_fine, wells_fine, upflow_cell_fine
-)
+def generate_data(G_t):
+
+    temp_t, pres_t, enth_t = data_handler_fine.split_obs(G_t)
+
+    cov_temp = (NOISE_LEVEL * np.max(temp_t)) ** 2 * np.eye(temp_t.size)
+    cov_pres = (NOISE_LEVEL * np.max(pres_t)) ** 2 * np.eye(pres_t.size)
+    cov_enth = (NOISE_LEVEL * np.max(enth_t)) ** 2 * np.eye(enth_t.size)
+
+    C_e = sparse.block_diag((cov_temp, cov_pres, cov_enth)).toarray()
+    y = np.random.multivariate_normal(G_t, C_e)
+
+    np.save(OBS_PATH, y)
+    np.save(COV_PATH, C_e)
+
+    return y, C_e
+
+def read_data():
+    y = np.load(OBS_PATH)
+    C_e = np.load(COV_PATH)
+    return y, C_e
+
+w_t, p_t, F_t, G_t, = generate_truth(mesh_fine, model_name_fine, 
+                                     wells_fine, upflow_cell_fine)
+
+y, C_e = generate_data(G_t)
