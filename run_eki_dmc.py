01005eec0fb428f33f8629aeef39b81acaa15c78
alexgdebeer <adeb970@aucklanduni.ac.nz>
2023-12-15 16:55:06 +1300

    Update scripts for running EKI and EnRML

--- run_eki_dmc.py
+++ run_eki_dmc.py
@@ -4,15 +4,16 @@ from setup_slice import *
 Np = n_blocks_crse + 1
 NF = n_blocks_crse + 2 * n_wells * (nt + 1)
 NG = len(y)
-Ne = 100
+Ne = 10
 
 ensemble = Ensemble(prior, generate_particle, get_result, Np, NF, NG, Ne)
 localiser = IdentityLocaliser()
+inflator = IdentityInflator()
 imputer = GaussianImputer()
 
-ws, ps, Fs, Gs, inds = run_eki_dmc(
-    ensemble, prior, y, C_e, Ne,
-    localiser, imputer, nesi=False)
+ws, ps, Fs, Gs, inds_succ = run_eki_dmc(
+    ensemble, prior, y, C_e,
+    localiser=localiser, inflator=inflator, imputer=imputer, nesi=False)
 
 fname = "eki_dmc.h5"
 results = {
@@ -20,6 +21,6 @@ results = {
     "ps": ps, 
     "Fs": Fs, 
     "Gs": Gs, 
-    "inds": inds
+    "inds_succ": inds_succ
 }
 save_results_eki(fname, results)
--- run_enrml.py
+++ run_enrml.py
@@ -3,20 +3,27 @@ from setup_slice import *
 
 Np = n_blocks_crse + 1
 NF = n_blocks_crse + 2 * n_wells * (nt + 1)
+NG = len(y)
 Ne = 100
 
+ensemble = Ensemble(prior, generate_particle, get_result, Np, NF, NG, Ne)
 localiser = IdentityLocaliser()
+inflator = IdentityInflator()
 imputer = GaussianImputer()
 
-ws, ps, Fs, Gs, Ss, lams, en_ind, inds = run_enrml(
-    F, G, prior, y, C_e, Np, NF, Ne, 
-    localiser=localiser, imputer=imputer)
+ws, ps, Fs, Gs, Ss, lams, en_ind, inds_succ = run_enrml(
+    ensemble, prior, y, C_e,
+    localiser=localiser, inflator=inflator, imputer=imputer, nesi=False)
 
 fname = "enrml.h5"
 results = {
-    "ws": ws, "ps": ps, 
-    "Fs": Fs, "Gs": Gs, 
-    "inds": inds, 
-    "Ss": Ss, "lams": lams
+    "ws": ws, 
+    "ps": ps, 
+    "Fs": Fs, 
+    "Gs": Gs, 
+    "inds_succ": inds_succ, 
+    "Ss": Ss, 
+    "lams": lams
 }
+
 save_results_enrml(fname, results, en_ind)
