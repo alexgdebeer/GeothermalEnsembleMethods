c7db3d69c569b372b95a513991769f561901c833
Alex de Beer <adeb970@aucklanduni.ac.nz>
2024-01-06 15:33:13 +1300

    Remove Matern field plotting scripts

--- scripts/enrml.py
+++
@@ -1,9 +0,0 @@
-import GeothermalEnsembleMethods as gm
-
-from setup_slice import *
-
-Ne = 100
-
-prob = gm.EnRMLProblem(f, g, prior, likelihood, Nf, Ne, i_max=30, loc_type="cycle")
-prob.run()
-prob.save_results(fname=f"data/enrml_{Ne}_loc_cycle_max_30_tsvd_99")
